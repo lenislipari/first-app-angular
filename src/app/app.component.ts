f390bfd432c5c35c5113a1f396f6b5f6c055b8e9
lenislipari <lenislipari@gmail.com>
2023-05-10 20:15:51 -0300

    complete app to lesson 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+   
+  <!-- <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section> -->
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
